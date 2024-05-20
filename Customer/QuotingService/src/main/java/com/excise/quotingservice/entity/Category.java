b36f91b632c06a56c3ae4301131e24550eb5353a
buiquang9999 <buiquang8627@gmail.com>
2024-05-20 18:43:08 +0700

    Customer.gitignore

--- Customer/QuotingService/src/main/java/com/excise/quotingservice/entity/Category.java
+++
@@ -1,7 +0,0 @@
-package com.excise.quotingservice.entity;
-
-public enum Category {
-    IPHONE,
-    ANDROID
-
-}
--- Customer/src/main/java/com/excise/customerserviceinterface/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.excise.customerserviceinterface;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-}
